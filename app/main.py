08e5121c44dfb25146e1881019bae0732fd07674
konduskarsuyash <suyashkonduskar27@gmail.com>
2024-10-27 09:33:26 +0530

    CORS part done

--- app/main.py
+++ app/main.py
@@ -8,10 +8,25 @@ from psycopg2.extras import RealDictCursor   #importing because after running th
 from . import models,schemas,utils
 from .database import engine,get_db
 from .routers import post, user,auth,vote
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost:5173",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
         
 app.include_router(user.router)
 app.include_router(post.router)
